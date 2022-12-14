2dea2c161e1246e636acc7f3fad47d0697a8c7b3
KokiTeramoto <koki.teramoto.pd@hitachi.com>
2022-12-14 11:56:28 +0900

    add package_test

---
+++ package_test/pack/pack.go
@@ -0,0 +1,9 @@
+package pack
+
+import (
+	"fmt"
+)
+
+func Pack() {
+	fmt.Println("pack func invoked")
+}
---
+++ package_test/sub.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"fmt"
+)
+
+func sub() {
+	fmt.Println("sub func invoked")
+}
